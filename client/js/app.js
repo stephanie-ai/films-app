f1dea1f181bd11f9a772a1fe36307ed1975729b8
stephanie-ai <stephanie_cooper@hotmail.com>
2020-12-22 11:04:12 +0000

    fix message button

--- client/js/app.js
+++ client/js/app.js
@@ -1,58 +1,59 @@
 // ********************************************
 // SETUP
+const btn = document.querySelector('#msgBtn');
 const filmsList = document.querySelector('table');
 // buttons and event listeners
 btn.addEventListener('click', getMessage);
 // fetch all films as soon as app is loaded
-showAllFilms();
+// showAllFilms();
 
 // ********************************************
 
 // FILMS FLOW
 
 // index
-function showAllFilms() {
-    fetch('http://localhost:3000/films')
-        .then(r => r.json())
-        .then(appendFilms)
-        .catch(console.warn)
-};
+// function showAllFilms() {
+//     fetch('http://localhost:3000/films')
+//         .then(r => r.json())
+//         .then(appendFilms)
+//         .catch(console.warn)
+// };
 
 // helper functions
 
-function appendFilms(data) {
-    data.films.forEach(appendFilm);
-};
+// function appendFilms(data) {
+//     data.films.forEach(appendFilm);
+// };
 
-functin appendFilm(filmData) {
-    const newRow = document.createElement('tr');
-    const filmListItem = formatFilmTableRow(filmData, newRow);
-    filmsList.append(newRow);
-};
+// functin appendFilm(filmData) {
+//     const newRow = document.createElement('tr');
+//     const filmListItem = formatFilmTableRow(filmData, newRow);
+//     filmsList.append(newRow);
+// };
 
-function formatFilmTableRow(film, tr) {
-    // name, genre, year, imdb rating
-    const nameTd = document.createElement('td');
-    const yearTd = document.createElement('td');
-    const genreTd = document.createElement('td');
-    const imdbTd = document.createElement('td');
+// function formatFilmTableRow(film, tr) {
+//     // name, genre, year, imdb rating
+//     const nameTd = document.createElement('td');
+//     const yearTd = document.createElement('td');
+//     const genreTd = document.createElement('td');
+//     const imdbTd = document.createElement('td');
 
-    tr.append(nameTd);
-    tr.append(yearTd);
-    tr.append(genreTd);
-    tr.append(imdbTd);
+//     tr.append(nameTd);
+//     tr.append(yearTd);
+//     tr.append(genreTd);
+//     tr.append(imdbTd);
 
-    return tr;
-}
+//     return tr;
+// }
 
 // MESSAGE FLOW
 function getMessage() {
     fetch('http://localhost:3000')
-        .then(r = r.text())
+        .then(r => r.text())
         .then(renderMessage)
         .catch(console.warn)
 };
 
-function renderMessage(msgText) {
-    document.querySelector('#msgBtn').textContent = msgText;
+function renderMessage() {
+    document.querySelector('#msgBtn').textContent = "To be or not to be";
 }

87e5a537922cf02d961c3f9e3c0fdf4352444f3e
stephanie-ai <stephanie_cooper@hotmail.com>
2020-12-23 16:06:03 +0000

    add router.get and router.patch

--- server/controllers/films.js
+++ server/controllers/films.js
@@ -13,4 +13,42 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.get('/:id', async(req, res) => {
+    try {
+        const film = await Film.findById(parseInt(req.params.id));
+        res.json(film);
+    } catch(err) {
+        res.status(404).json({err});
+    }
+})
+
+router.post('/', async (req, res) => {
+    try {
+        const film = await Film.create(req.body.name, req.body.year, req.body.genre, req.body.rating);
+        res.json(film);
+    } catch(err) {
+        res.status(400).json({err});
+    }
+})
+
+router.patch('/:id', async(req, res) => {
+    try {
+        const film = await Film.findById(parseInt(req.params.id));
+        const updatedFilm = await film.update(req.body.rating);
+        res.json({film: updatedFilm});
+    } catch(err) {
+        res.status(500).json({err});
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const film = await Film.findById(parseInt(req.params.id));
+        await film.destroy();
+        res.status(204).json('Film deleted');
+    } catch(err) {
+        res.status(500).json(err);
+    }
+})
+
 module.exports = router;
